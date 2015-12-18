1e44fd5aef441051da1d09e6322256d020fe1499
KOTARO SUTO <ng1a5ss43@gmail.com>
2015-12-18 17:13:02 +0900

    Add Custom Cell

---
+++ iTunesViewer/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  iTunesViewer
+//
+//  Created by Kotaro Suto on 2015/12/18.
+//  Copyright © 2015年 Kotaro Suto. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
