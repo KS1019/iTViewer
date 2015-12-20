195b1122edeb4b2426c7cbdb88d63f02bbae5c01
KOTARO SUTO <ng1a5ss43@gmail.com>
2015-12-20 17:42:15 +0900

    Add cell function

--- iTunesViewer/ViewController.swift
+++ iTunesViewer/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getDataOfApps()
+        //self.getDataOfApps()
+        
         // Do any additional setup after loading the view, typically from a nib.
 
     }
@@ -45,7 +46,28 @@ class ViewController: UIViewController {
     // MARK: - UICollectionViewDelegate Protocol
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell:CustomCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CustomCollectionViewCell
-            return cell
+        var titles: [String] = [ "A", "F", "G", "L" ]
+        let index = (Int)(arc4random_uniform(4))
+        print("\(titles[index])")
+        let params = ["term":titles[index], "country":"jp", "media":"software", "entity":"software", "limit":"1", "lang":"ja_jp"]
+        Alamofire.request(.POST, "https://itunes.apple.com/search", parameters: params)
+            .responseJSON{ response in
+                print("\(response.result.value)")
+                let json = JSON(response.result.value!)
+                cell.labelOfAppName.text = json["results"][0]["trackName"].string
+                let url = NSURL(string:json["results"][0]["screenshotUrls"].string!)
+                let req = NSURLRequest(URL:url!)
+                
+                NSURLConnection.sendAsynchronousRequest(req, queue:NSOperationQueue.mainQueue()){(res, data, err) in
+                    let image = UIImage(data:data!)
+                    // 画像に対する処理 (UcellのUIImageViewに表示する等)
+                    cell.imageViewOfScreenShot.image = image
+                }
+
+                
+        }
+        
+        return cell
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
