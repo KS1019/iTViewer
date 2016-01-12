361e343f142e86f9baeedc479c854d97d1585a18
KOTARO SUTO <ng1a5ss43@gmail.com>
2016-01-12 21:24:15 +0900

    Add comment

--- iTunesViewer/ViewController.swift
+++ iTunesViewer/ViewController.swift
@@ -52,6 +52,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //
     }
     
     
